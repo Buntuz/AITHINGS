c4951e8c279adb708942adac995ae94ca81b9fc8
Buntu Ntozake <buntuntozake@Buntus-MacBook-Air.local>
2024-03-09 11:42:42 +0200

    initial commit

--- lib/information.tsx
+++ lib/information.tsx
@@ -3,10 +3,17 @@ import React from 'react'
 const information = () => {
   return (
     <div>
+        {/*/https://www.youtube.com/watch?v=Ahwoks_dawU*/}
+
+        {/*https://github.com/adrianhajdin/ai_saas_app/blob/main/app/(root)/layout.tsx*/}
       {/*  Mango DB  username:buntuntozake, password: wqCvW5AegFz1Pq7I */}
       {/* https://cloud.mongodb.com/v2/65ea16b51abf8d6e1d08473a#/setup/access?includeToast=true*/}
 
       {/* mongodb+srv://buntuntozake:<password>@cluster0.ugd9wy0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0*/}
+      {/* https://aithings.vercel.app/ */}
+      {/* https://ui.shadcn.com/docs/components/sheet */}
+      {/* https://clerk.com/docs/quickstarts/nextjs?_gl=1*sbdkqp*_gcl_au*MTAyNjQ0Njc5OC4xNzA5NTUwMjAw*_ga*MTA1MjgwMTc4MS4xNzA5NTUwMTk5*_ga_1WMF5X234K*MTcwOTU1MDE5OS4xLjEuMTcwOTU1MDgxMS4wLjAuMA.. */}
+
     </div>
   )
 }
